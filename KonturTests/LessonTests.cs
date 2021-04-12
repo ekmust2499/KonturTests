59ffc7e02c36cc3deff62de294f32bb7643d17e3
Ekaterina Mustafina <katiammm0204@mail.ru>
2021-04-12 12:44:24 +0500

    Добавлены тесты на страницу с попугайчиками

---
+++ KonturTests/LessonTests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace KonturTests
+{
+    public class LessonTests
+    {
+        public ChromeDriver driver;
+        public WebDriverWait wait;
+
+      [SetUp]
+        public void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            driver = new ChromeDriver(options);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5)); //явные ожидания
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); //неявные ожидания
+        }
+
+        [Test]
+        public void Wikipedia_Search_TitleIsCorrect()
+        {
+            //перешли на страницу
+            driver.Navigate().GoToUrl("https://ru.wikipedia.org/");
+
+            //ищем элементы
+            var search = driver.FindElement(By.Name("search"));
+            var searchButton = driver.FindElement(By.Name("go"));
+
+            //взаимодействуем с элементами
+            search.SendKeys("Selenium");
+            searchButton.Click();
+
+            //проверки в тесте
+            Assert.IsTrue(driver.Title.Contains("Selenium — Википедия"), "Неверный заголовок страницы при переходе из поиска");
+        }
+
+   
+        private By emailInputLocator = By.ClassName("email");
+        private By buttonLocator = By.Id("write-to-me");
+        private By emailResultLocator = By.Name("result-email");
+        private By anotherEmailLinkLocator = By.LinkText("указать другой e-mail");
+
+        [Test]
+        public void ComputerSite_FormFillWithEmail_Success()
+        {
+            //перешли на страницу
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/selenium-lesson");
+
+            //заполнить форму и кликнуть
+            var expectedEmail = "test@mail.ru";
+            driver.FindElement(emailInputLocator).SendKeys(expectedEmail);
+            driver.FindElement(buttonLocator).Click();        
+
+            //проверки
+            Assert.AreEqual(expectedEmail, driver.FindElement(emailResultLocator).Text, "Сделали заявку не на тот e-mail");
+        }
+
+        [Test]
+        public void ComputerSite_ClickAnotherEmail_EmailInputIsEmpty()
+        {
+            //перешли на страницу
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/selenium-lesson");
+
+            //заполнить форму и кликнуть
+            driver.FindElement(emailInputLocator).SendKeys("test@mail.ru");
+            driver.FindElement(buttonLocator).Click();
+            driver.FindElement(anotherEmailLinkLocator).Click();
+
+            //проверки
+            Assert.AreEqual(string.Empty, driver.FindElement(emailInputLocator).Text, "После клика по ссылке поле не очистилось");
+            Assert.IsTrue(driver.FindElements(anotherEmailLinkLocator).Count == 0, "Не исчезла ссылка для ввода другого e-mail");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ KonturTests/PracticeTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace KonturTests
+{
+    public class PracticeTests
+    {
+        public ChromeDriver driver;
+        public WebDriverWait wait;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            driver = new ChromeDriver(options);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5)); //явные ожидания
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); //неявные ожидания
+        }
+
+        private By emailInputLocator = By.Name("email");
+        private By buttonLocator = By.Id("sendMe");
+        private By radioButtonBoyLocator = By.Id("boy");
+        private By radioButtonGirlLocator = By.Id("girl");
+        private By emailResultLocator = By.ClassName("your-email");
+        private By anotherEmailLinkLocator = By.Id("anotherEmail");
+        private By formErrorLocator = By.ClassName("form-error");
+        private By resultTextLocator = By.ClassName("result-text");
+        private String expectedEmail = "test@mail.ru";
+        private String url = "https://qa-course.kontur.host/selenium-practice";
+
+        [Test]
+        public void ParrotNameSite_FormFillWithEmail_CorrectEmail()
+        {
+            driver.Navigate().GoToUrl(url);
+
+            driver.FindElement(emailInputLocator).SendKeys(expectedEmail);
+            driver.FindElement(buttonLocator).Click();
+
+            Assert.AreEqual(expectedEmail, driver.FindElement(emailResultLocator).Text, "Сделали заявку не на тот e-mail");
+        }
+        [Test]
+        public void ParrotNameSite_FillEmailChooseGirl_CorrectGender()
+        {
+            driver.Navigate().GoToUrl(url);
+
+            driver.FindElement(emailInputLocator).SendKeys(expectedEmail);
+            driver.FindElement(radioButtonGirlLocator).Click();
+            driver.FindElement(buttonLocator).Click();
+
+            Assert.IsTrue(driver.FindElement(resultTextLocator).Text.Contains("девочки"), "Сделали заявку c другим полом");
+        }
+
+        [Test]
+        public void ParrotNameSite_ClickAnotherEmailLink_EmailInputIsEmpty()
+        {
+            driver.Navigate().GoToUrl(url);
+
+            driver.FindElement(emailInputLocator).SendKeys(expectedEmail);
+            driver.FindElement(buttonLocator).Click();
+            driver.FindElement(anotherEmailLinkLocator).Click();
+
+            Assert.AreEqual(string.Empty, driver.FindElement(emailInputLocator).Text, "После клика по ссылке поле не очистилось");
+        }
+        [Test]
+        public void ParrotNameSite_ClickAnotherEmailLink_RemovedAnotherEmailLink()
+        {
+            driver.Navigate().GoToUrl(url);
+
+            driver.FindElement(emailInputLocator).SendKeys(expectedEmail);
+            driver.FindElement(buttonLocator).Click();
+            driver.FindElement(anotherEmailLinkLocator).Click();
+
+            Assert.IsFalse(driver.FindElement(anotherEmailLinkLocator).Displayed, "Не исчезла ссылка для ввода другого e-mail");
+        }
+
+        [Test]
+        public void ParrotNameSite_ChangeGenderBeforeClickAnotherEmailLink_GenderSaved()
+        {
+            driver.Navigate().GoToUrl(url);
+
+            driver.FindElement(emailInputLocator).SendKeys(expectedEmail);
+            driver.FindElement(radioButtonBoyLocator).Click();
+            driver.FindElement(buttonLocator).Click();
+            driver.FindElement(radioButtonGirlLocator).Click();
+            driver.FindElement(anotherEmailLinkLocator).Click();
+
+            Assert.IsTrue(driver.FindElement(radioButtonGirlLocator).Selected, "Произошла смена пола");
+        }
+
+        [Test]
+        public void ParrotNameSite_SendEmptyEmailField_ErrorOutput()
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.FindElement(radioButtonGirlLocator).Click();
+            driver.FindElement(buttonLocator).Click();
+
+            Assert.AreEqual("Введите email", driver.FindElement(formErrorLocator).Text, "При отправке пустого e-mail нет сообщения об ошибке");
+        }
+
+        [Test]
+        public void ParrotNameSite_SendIncorrectEmail_ErrorOutput()
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.FindElement(radioButtonGirlLocator).Click();
+            driver.FindElement(emailInputLocator).SendKeys("test");
+            driver.FindElement(buttonLocator).Click();
+
+            Assert.AreEqual("Некорректный email", driver.FindElement(formErrorLocator).Text, "При отправке некорректногоо e-mail нет сообщения об ошибке");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
